da4b768a3f875119fd4fd0b0c4b13a7f71c15b7f
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-11-25 15:02:08 +0500

    Initial commit

---
+++ src/main/kotlin/kh/farrukh/espielspringsecurity/EspielSpringSecurityApplication.kt
@@ -0,0 +1,11 @@
+package kh.farrukh.espielspringsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EspielSpringSecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<EspielSpringSecurityApplication>(*args)
+}
---
+++ src/test/kotlin/kh/farrukh/espielspringsecurity/EspielSpringSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package kh.farrukh.espielspringsecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EspielSpringSecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
