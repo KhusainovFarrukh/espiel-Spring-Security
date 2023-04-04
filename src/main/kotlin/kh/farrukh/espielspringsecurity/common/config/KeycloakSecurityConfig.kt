bebd8c8ea85477f1e36b8ab6be3ab42f91adbe22
f_khusainov <f.khusainov@uzinfocom.uz>
2023-04-04 06:47:17 +0500

    Code refactor

--- src/main/kotlin/kh/farrukh/espielspringsecurity/common/config/KeycloakSecurityConfig.kt
+++ src/main/kotlin/kh/farrukh/espielspringsecurity/common/config/KeycloakSecurityConfig.kt
@@ -1,38 +1,23 @@
-package kh.farrukh.espielspringsecurity.keycloak
+package kh.farrukh.espielspringsecurity.common.config
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootProperties
-import org.keycloak.adapters.springboot.KeycloakSpringBootProperties.SecurityConstraint
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
-import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter
-import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
-import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter
 import org.springframework.security.web.util.matcher.RequestMatcher
 
 @KeycloakConfiguration
-@EnableConfigurationProperties(KeycloakSpringBootProperties::class)
 class KeycloakSecurityConfig(
-    private val keycloakSpringBootProperties: KeycloakSpringBootProperties,
+    private val keycloakProperties: KeycloakSpringBootProperties,
 ) : KeycloakWebSecurityConfigurerAdapter() {
 
     @Autowired
     fun configureGlobal(auth: AuthenticationManagerBuilder) {
-//        val grantedAuthorityMapper = SimpleAuthorityMapper()
-//        grantedAuthorityMapper.setPrefix("ROLE_")
-
-        val keycloakAuthenticationProvider = keycloakAuthenticationProvider()
-//        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(grantedAuthorityMapper)
-        auth.authenticationProvider(keycloakAuthenticationProvider)
+        auth.authenticationProvider(keycloakAuthenticationProvider())
     }
 
     override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
@@ -41,28 +26,23 @@ class KeycloakSecurityConfig(
 
     override fun configure(http: HttpSecurity) {
         super.configure(http)
-        //        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter())
-        // Require authentication for all requests
+
         val publicPaths = getPublicPaths()
-        http.csrf().disable().cors().disable().authorizeHttpRequests()
+        http
+            .csrf().disable()
+            .cors().disable()
+            .authorizeHttpRequests()
             .requestMatchers(RequestMatcher { request -> publicPaths.contains(request.servletPath) })
             .permitAll()
             .anyRequest()
             .authenticated()
-
-        http.csrf().disable()
-
-        //http.oauth2Client();
-
-        // Allow showing pages within a frame
-        http.headers().frameOptions().sameOrigin()
     }
 
     //TODO find better way
     //TODO test with/without wildcards (/*, /**)
     private fun getPublicPaths(): List<String> {
         val publicPaths = mutableListOf<String>()
-        keycloakSpringBootProperties.securityConstraints
+        keycloakProperties.securityConstraints
             .filter { constraint -> constraint.authRoles.size == 0 }
             .forEach { constraint ->
                 constraint.securityCollections.forEach { collection ->
